5efdc1fc08de4c44ca94069a345366634425730f
ussef11 <khiraouiyoussef@gmail.com>
2023-07-24 13:05:15 +0100

    update by ussef

--- src/camtest.js
+++ src/camtest.js
@@ -1,17 +1,16 @@
 import React, { useState, useRef, useCallback } from "react";
 import Webcam from "react-webcam";
-import Switch from "./media/switch.png";
-import Camera from "./media/camera.png";
+import Switch from "./media/switch.png"
+import Camera from "./media/camera.png"
 
 const FACING_MODE_USER = "user";
 const FACING_MODE_ENVIRONMENT = "environment";
-const FLASH_MODE_ON = "on";
-const FLASH_MODE_OFF = "off";
+
 export default function Camtest() {
   const webcamRef = useRef(null);
   const [img, setImg] = useState("");
   const [facingMode, setFacingMode] = useState(FACING_MODE_USER);
-  const [flashMode, setFlashMode] = useState(FLASH_MODE_OFF);
+
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImg(imageSrc);
@@ -28,22 +27,16 @@ export default function Camtest() {
     }
   };
 
- 
-
   const videoConstraints = {
     facingMode: facingMode,
-    audio: false,
-    aspectRatio: 1.777777778,
-    frameRate: { max: 30 },
-    // Add flashMode option
-    flashMode: flashMode
-  };
 
-  const handleFlash = useCallback(() => {
-    setFlashMode((prevState) =>
-      prevState === FLASH_MODE_ON ? FLASH_MODE_OFF : FLASH_MODE_ON
-    );
-  }, []);
+    height: 720,
+    audio:true,
+
+    frameRate: 30,
+    advanced: [{ torch: true }]
+
+  };
 
   const handleClick = useCallback(() => {
     setFacingMode((prevState) =>
@@ -51,55 +44,49 @@ export default function Camtest() {
         ? FACING_MODE_ENVIRONMENT
         : FACING_MODE_USER
     );
+
   }, []);
 
   console.log(facingMode + videoConstraints);
-
 
   return (
     <>
       <div className="webcam-container">
         <div className="webcam-img">
-          <div style={{ textAlign: "center" }}>
-            {" "}
-            <img onClick={handleClick} src={Switch} />
-          </div>
+      <div style={{textAlign:"center"}}>  <img   onClick={handleClick}  src={Switch}  /></div> 
           {img === "" ? (
-            <>
-              {" "}
-              <Webcam
-                className="webcam"
-                audio={false}
-                ref={webcamRef}
-                screensshotFormat="image/jpeg"
-                videoConstraints={videoConstraints}
-                screenshotQuality={1}
-               
-                style={{ width: "100%" }}
-              />
-              <div style={{ textAlign: "center" }}>
-                {" "}
-                <img className="dd" onClick={capture} src={Camera} />{" "}
-              </div>{" "}
-            </>
-          ) : (
-            <>
-              <img
-                src={img}
-                alt="Scan"
-                style={{ width: "100%", height: "auto" }}
-              />
+      <>      <Webcam
+              className="webcam"
+              audio={false}
+              ref={webcamRef}
+              screensshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+              screenshotQuality={1}
+              scale={5}
 
-              <div style={{ textAlign: "center" }}>
-                <img className="dd" onClick={() => setImg("")} src={Camera} />{" "}
-              </div>
+              style={{width:"100%"}}
+             
+            />
+            <div style={{textAlign:"center"}}>      <img  className="dd" onClick={capture}  src={Camera}  /> </div>  </> 
+          ) : (
+            <> 
+            <img
+              src={img}
+              alt="Scan"
+              style={{ width: "100%", height: "auto" }}
+            />
+      
+            
+      <div style={{textAlign:"center"}}> <img  className="dd" onClick={() => setImg("")} src={Camera}  /> </div>
+            
             </>
           )}
         </div>
-        <button onClick={handleFlash}>
-          {flashMode === FLASH_MODE_ON ? "Turn Flash Off" : "Turn Flash On"}
-        </button>
+       
+
         {img && <button onClick={downloadImage}>Download</button>}
+
+        
       </div>
     </>
   );
