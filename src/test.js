6f627c11f4fe7eea533b6805766e7d384fcfc9f0
ussef11 <khiraouiyoussef@gmail.com>
2023-07-24 14:53:27 +0100

    update by ussef

--- src/test.js
+++ src/test.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 const Test = () => {
   const [torchSupported, setTorchSupported] = useState(false);
   const [torchOn, setTorchOn] = useState(false);
+  const videoRef = React.useRef(null);
 
   useEffect(() => {
     // Test browser support for mediaDevices
@@ -10,74 +11,65 @@ const Test = () => {
 
     if (SUPPORTS_MEDIA_DEVICES) {
       // Get the environment camera (usually the second one)
-      navigator.mediaDevices.enumerateDevices().then((devices) => {
-        const cameras = devices.filter((device) => device.kind === "videoinput");
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then((devices) => {
+          const cameras = devices.filter(
+            (device) => device.kind === "videoinput"
+          );
 
-        if (cameras.length === 0) {
-          console.log("No camera found on this device.");
-        }
+          if (cameras.length === 0) {
+            console.log("No camera found on this device.");
+          }
 
-        // Create stream and get video track
-        navigator.mediaDevices
-          .getUserMedia({
-            video: {
-              facingMode: "environment",
-            },
-          })
-          .then((stream) => {
-            const track = stream.getVideoTracks()[0];
+          // Create stream and get video track
+          navigator.mediaDevices
+            .getUserMedia({
+              video: {
+                facingMode: "environment",
+              },
+            })
+            .then((stream) => {
+              videoRef.current.srcObject = stream;
 
-            // Create image capture object and get camera capabilities
-            const imageCapture = new ImageCapture(track);
-            imageCapture
-              .getPhotoCapabilities()
-              .then((capabilities) => {
-                // Check if torch is supported
-                const supportedTorch =
-                  !!capabilities.torch ||
-                  ("fillLightMode" in capabilities &&
-                    capabilities.fillLightMode.length !== 0 &&
-                    capabilities.fillLightMode !== "none");
-
-                setTorchSupported(supportedTorch);
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      });
+              // Check if torch is supported
+              const supportedTorch = !!stream.getVideoTracks()[0].applyConstraints;
+              setTorchSupported(supportedTorch);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     } else {
       console.log("MediaDevices not supported in this browser.");
     }
   }, []);
 
   const handleToggleTorch = () => {
-    try {
-      navigator.mediaDevices.enumerateDevices().then((devices) => {
-        const cameras = devices.filter((device) => device.kind === "videoinput");
-        const track = cameras[0]?.getVideoTracks()[0];
+    const videoTrack = videoRef.current.srcObject?.getVideoTracks()[0];
 
-        if (track) {
-          track.applyConstraints({
-            advanced: [
-              {
-                torch: !torchOn,
-              },
-            ],
-          });
-          setTorchOn(!torchOn);
-        }
-      });
-    } catch (err) {
-      console.log(err);
+    if (videoTrack && torchSupported) {
+      try {
+        videoTrack.applyConstraints({
+          advanced: [{ torch: !torchOn }],
+        });
+        setTorchOn(!torchOn);
+      } catch (err) {
+        console.log(err);
+      }
     }
   };
 
   return (
     <div>
+      <video
+        ref={videoRef}
+        autoPlay
+        style={{ width: "100%", height: "auto" }}
+      ></video>
       {torchSupported ? (
         <button onClick={handleToggleTorch}>
           {torchOn ? "Turn Off Torch" : "Turn On Torch"}
