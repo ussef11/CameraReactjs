537980cf6d9a5106d483aef43df514a49c945815
ussef11 <khiraouiyoussef@gmail.com>
2023-07-24 16:29:15 +0100

    update by ussef

--- src/camtest.js
+++ src/camtest.js
@@ -48,18 +48,68 @@ export default function Camtest() {
     );
   }, []);
 
-  useEffect(() => {
-    // Check if the torch feature is supported by the device
-    navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: "environment", advanced: [{ torch: true }] } })
-      .then(() => setTorchSupported(true))
-      .catch(() => setTorchSupported(false));
+
+
+
+useEffect(() => {
+    // Test browser support for mediaDevices
+    const SUPPORTS_MEDIA_DEVICES = "mediaDevices" in navigator;
+
+    if (SUPPORTS_MEDIA_DEVICES) {
+      // Get the environment camera (usually the second one)
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then((devices) => {
+          const cameras = devices.filter(
+            (device) => device.kind === "videoinput"
+          );
+
+          if (cameras.length === 0) {
+            console.log("No camera found on this device.");
+          }
+
+          // Create stream and get video track
+          navigator.mediaDevices
+            .getUserMedia({
+              video: {
+                facingMode: "environment",
+              },
+            })
+            .then((stream) => {
+              videoRef.current.srcObject = stream;
+
+              // Check if torch is supported
+              const supportedTorch = !!stream.getVideoTracks()[0].applyConstraints;
+              setTorchSupported(supportedTorch);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      console.log("MediaDevices not supported in this browser.");
+    }
   }, []);
 
   const handleToggleTorch = () => {
-    setTorchOn((prevTorchOn) => !prevTorchOn);
+    const videoTrack = videoRef.current.srcObject?.getVideoTracks()[0];
+
+    if (videoTrack && torchSupported) {
+      try {
+        videoTrack.applyConstraints({
+          advanced: [{ torch: !torchOn }],
+        });
+        setTorchOn(!torchOn);
+      } catch (err) {
+        console.log(err);
+      }
+    }
   };
 
+  
   return (
     <>
       <div className="webcam-container">
