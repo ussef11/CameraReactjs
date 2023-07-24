7955159e32bfeb798639bf8d3ba6cc657b61c9ff
ussef11 <khiraouiyoussef@gmail.com>
2023-07-24 13:14:14 +0100

    update by ussef

--- src/test.js
+++ src/test.js
@@ -1,92 +1,92 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 
-export default function WebcamCapture() {
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
-  const [img, setImg] = useState("");
-  const [facingMode, setFacingMode] = useState("user");
-  const [flashOn, setFlashOn] = useState(false);
+const Test = () => {
+  const [torchSupported, setTorchSupported] = useState(false);
+  const [torchOn, setTorchOn] = useState(false);
 
-  const capture = useCallback(() => {
-    const video = webcamRef.current;
-    const canvas = canvasRef.current;
+  useEffect(() => {
+    // Test browser support for mediaDevices
+    const SUPPORTS_MEDIA_DEVICES = "mediaDevices" in navigator;
 
-    if (video && canvas) {
-      const context = canvas.getContext("2d");
-      context.drawImage(
-        video,
-        0,
-        0,
-        canvas.width,
-        canvas.height
-      );
+    if (SUPPORTS_MEDIA_DEVICES) {
+      // Get the environment camera (usually the second one)
+      navigator.mediaDevices.enumerateDevices().then((devices) => {
+        const cameras = devices.filter((device) => device.kind === "videoinput");
 
-      // Get the image data from the canvas and set it in state
-      const imageSrc = canvas.toDataURL("image/jpeg");
-      setImg(imageSrc);
+        if (cameras.length === 0) {
+          console.log("No camera found on this device.");
+        }
+
+        // Create stream and get video track
+        navigator.mediaDevices
+          .getUserMedia({
+            video: {
+              facingMode: "environment",
+            },
+          })
+          .then((stream) => {
+            const track = stream.getVideoTracks()[0];
+
+            // Create image capture object and get camera capabilities
+            const imageCapture = new ImageCapture(track);
+            imageCapture
+              .getPhotoCapabilities()
+              .then((capabilities) => {
+                // Check if torch is supported
+                const supportedTorch =
+                  !!capabilities.torch ||
+                  ("fillLightMode" in capabilities &&
+                    capabilities.fillLightMode.length !== 0 &&
+                    capabilities.fillLightMode !== "none");
+
+                setTorchSupported(supportedTorch);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
+    } else {
+      console.log("MediaDevices not supported in this browser.");
     }
   }, []);
 
-  const downloadImage = () => {
-    if (img) {
-      const a = document.createElement("a");
-      a.href = img;
-      a.download = "captured_image.jpg";
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
+  const handleToggleTorch = () => {
+    try {
+      navigator.mediaDevices.enumerateDevices().then((devices) => {
+        const cameras = devices.filter((device) => device.kind === "videoinput");
+        const track = cameras[0]?.getVideoTracks()[0];
+
+        if (track) {
+          track.applyConstraints({
+            advanced: [
+              {
+                torch: !torchOn,
+              },
+            ],
+          });
+          setTorchOn(!torchOn);
+        }
+      });
+    } catch (err) {
+      console.log(err);
     }
   };
-
-  const handleClick = useCallback(() => {
-    setFacingMode((prevState) => (prevState === "user" ? "environment" : "user"));
-  }, []);
-
-  const handleFlash = useCallback(() => {
-    // Add code here to toggle flash
-    // Note: Controlling the flash may not work in all environments
-    setFlashOn((prevFlashOn) => !prevFlashOn);
-  }, []);
-
-  const constraints = {
-    video: {
-      facingMode: { exact: facingMode },
-      width: 1280,
-      height: 720,
-      frameRate: 30,
-      // Add flash control
-      advanced: [{ torch: flashOn }]
-    }
-  };
-
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then((stream) => {
-      webcamRef.current.srcObject = stream;
-    })
-    .catch((error) => {
-      console.error("Error accessing webcam:", error);
-    });
 
   return (
-    <>
-      <div className="webcam-container">
-        <div className="webcam-img">
-          {img === "" ? (
-            <video ref={webcamRef} autoPlay />
-          ) : (
-            <img src={img} alt="Scan" style={{ width: "500px", height: "auto" }} />
-          )}
-        </div>
-        <button onClick={capture}>Capture</button>
-        {img && <button onClick={downloadImage}>Download</button>}
-        <button onClick={handleFlash}>
-          {flashOn ? "Turn Flash Off" : "Turn Flash On"}
+    <div>
+      {torchSupported ? (
+        <button onClick={handleToggleTorch}>
+          {torchOn ? "Turn Off Torch" : "Turn On Torch"}
         </button>
-        <button onClick={handleClick}>Switch camera</button>
-      </div>
-      {/* Hidden canvas to capture the image */}
-      <canvas ref={canvasRef} style={{ display: "none" }} />
-    </>
+      ) : (
+        <p>No torch found</p>
+      )}
+    </div>
   );
-}
+};
+
+export default Test;
